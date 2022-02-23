2190e8b67616c4800e6b019e9c730674fd77edae
Karleen Julien <karleenjulien@gmail.com>
2022-02-23 16:51:44 -0500

    Hello World in NetBeans

---
+++ HelloWorld/src/main/java/com/KJ/helloworld/Hello.java
@@ -0,0 +1,13 @@
+package com.KJ.helloworld;
+
+/**
+ * @author: Karleen Julien
+ * 
+ */
+
+public class Hello {
+    
+    public static void main (String[] args){
+        System.out.println("Hello, K");
+    }
+}
